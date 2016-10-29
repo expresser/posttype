f5b1e57cb411e0da89c5b11ceb61a7db88609b88
Graham Blevins <hgblevins@gmail.com>
2016-10-29 16:46:44 -0400

    add editorconfig, cleanup

--- Native.php
+++ Native.php
@@ -6,7 +6,6 @@ abstract class Native extends Base
 {
     public static function registerPostType()
     {
-
-    // Do not implement registerPostType for native or exisitng types
+        // Do not implement registerPostType for native or exisitng types
     }
 }
